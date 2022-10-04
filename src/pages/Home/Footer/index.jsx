0d4f2cc69735f1c07139ca34735a334690bb0289
daniaharoun2000 <111414950+daniaharoun2000@users.noreply.github.com>
2022-10-04 13:51:05 +0300

    updated

--- src/pages/Home/Footer/index.jsx
+++ src/pages/Home/Footer/index.jsx
@@ -1,22 +1,29 @@
-// Footer Index by Dania Haroun !!!
+// Footer Index.js by Dania Haroun !!!
 
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import "./Footer.scss"
+//// start of Footer function ///
 
 function Footer() {
   return (
     <div className="footercover bg-cat mb-5 mb-md-0 ">
       <section >
+              {/* Section 1 section */}
         <Section1 />
       </section>
    <section>
+                  {/* Section 2 section */}
         <Section2 />
       </section>
-      <section><Section3 /></section> 
+      <section>
+                      {/* Section 3 section */}
+        <Section3 />
+        </section> 
     </div>
   );
 }
 
-export default Footer;
+export default Footer; //export Footer function to use it when It needed
+//// End of Footer function ///
